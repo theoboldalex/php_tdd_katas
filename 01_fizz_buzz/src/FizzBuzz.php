c7a027e58cc933f8df5c94f214a9741aa6b9024b
Alex Theobold <alex.theobold@sykescottages.co.uk>
2024-10-11 18:20:42 +0100

    My solution with basic refactoring

--- 01_fizz_buzz/src/FizzBuzz.php
+++ 01_fizz_buzz/src/FizzBuzz.php
@@ -2,6 +2,35 @@
 
 namespace App;
 
+use InvalidArgumentException;
+
 class FizzBuzz
 {
+    private const RESULT_MAP = [
+        3 => 'Fizz',
+        5 => 'Buzz',
+        7 => 'Bar'
+    ];
+
+    public function calculate(int $number = 1): string
+    {
+        if ($number < 1) {
+            throw new InvalidArgumentException('Argument must be a positive integer. Given: ' . $number);
+        }
+
+        $result = null;
+
+        foreach (self::RESULT_MAP as $index => $resultPart) {
+            if ($this->isDivisbleBy($number, $index)) {
+                $result .= $resultPart;
+            }
+        }
+
+        return $result ?? (string) $number;
+    }
+
+    private function isDivisbleBy(int $dividend, int $divisor): bool
+    {
+        return $dividend % $divisor === 0;
+    }
 }
--- 01_fizz_buzz/tests/FizzBuzzTest.php
+++ 01_fizz_buzz/tests/FizzBuzzTest.php
@@ -2,14 +2,43 @@
 
 namespace App\Tests;
 
-use PHPUnit\Framework\Attributes\CoversNothing;
+use App\FizzBuzz;
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
-#[CoversNothing()]
+#[CoversClass('FizzBuzz')]
 class FizzBuzzTest extends TestCase
 {
-    public function test_the_truth(): void
+    public function test_returns_an_error_if_input_is_negative_integer(): void
     {
-        $this->assertTrue(true);
+        $this->expectException(InvalidArgumentException::class);
+        $sut = new FizzBuzz;
+        $sut->calculate(-1);
+    }
+
+    #[DataProvider('useCaseProvider')]
+    public function test_it_calculates_fizzbuzz_inputs(
+        int $input,
+        string $expected
+    ): void
+    {
+        $sut = new FizzBuzz;
+        $result = $sut->calculate($input);
+        $this->assertEquals($expected, $result);
+    }
+
+    public static function useCaseProvider(): array
+    {
+        return [
+            'it returns one given one' => [1, "1"],
+            'it returns two given two' => [2, "2"],
+            'it returns Fizz given three' => [3, "Fizz"],
+            'it returns Buzz given five' => [5, "Buzz"],
+            'it returns Buzz given five' => [15, "FizzBuzz"],
+            'it returns Bar given one hundred and five' => [7, "Bar"],
+            'it returns FizzBuzzBar given one hundred and five' => [105, "FizzBuzzBar"],
+        ];
     }
 }
