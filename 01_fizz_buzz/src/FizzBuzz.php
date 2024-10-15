30ac16971fe73c80a2452ff8e88ee09e0db88618
Steven Roberts <steve.roberts@sykescottages.co.uk>
2024-10-15 10:19:30 +0100

    FizzBuzz TDD Kata - SJR

--- 01_fizz_buzz/src/FizzBuzz.php
+++ 01_fizz_buzz/src/FizzBuzz.php
@@ -4,4 +4,14 @@ namespace App;
 
 class FizzBuzz
 {
+    public function fizzBuzz(int $number) : string {
+        return ($number % 7 === 0 && $number % 5 === 0 && $number % 3 === 0) ? 'FizzBuzzBar' :
+                    (($number % 5 === 0 && $number % 3 === 0) ? 'FizzBuzz' :
+                        ($number % 3 === 0 ? "Fizz" :
+                            ($number % 5 === 0 ? "Buzz" :
+                                ($number % 7 === 0 ? "Bar" : "")
+                            )
+                        )
+                    );
+    }
 }
